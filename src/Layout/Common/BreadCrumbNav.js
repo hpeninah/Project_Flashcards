1f0ca6be3cc7a9cf1dd18c39413d9a46db2b58d4
Hannah Mulato <72578792+hpeninah@users.noreply.github.com>
2021-03-02 18:31:24 -0500

    Updating line 8 from class to className

--- src/Layout/Common/BreadCrumbNav.js
+++ src/Layout/Common/BreadCrumbNav.js
@@ -5,7 +5,7 @@ export default function BreadCrumbNav({ pageName = "Page Name", link = "", linkN
     return (
         <>
             <div className="row">
-                <Link className="m-1" to="/"><i class="bi bi-house m-1"></i>Home</Link>
+                <Link className="m-1" to="/"><i className="bi bi-house m-1"></i>Home</Link>
                 {link !== "" ? <p className="m-1 text-info">/</p> : ""}
                 <Link className={link !== "" ? "m-1" : ""} to={link}>{linkName}</Link>
                 <p className="m-1 text-info">/ {pageName}</p>
